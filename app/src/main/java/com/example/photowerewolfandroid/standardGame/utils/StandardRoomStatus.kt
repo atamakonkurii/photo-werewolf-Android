3954f3a2065b0a219b52046f30028bcbb7c6d33b
atamakonkurii <morisudesu@gmail.com>
2023-05-13 15:03:52 +0900

    Preview画面を追加

---
+++ app/src/main/java/com/example/photowerewolfandroid/standardGame/utils/StandardRoomStatus.kt
@@ -0,0 +1,10 @@
+package com.example.photowerewolfandroid.standardGame.utils
+
+enum class StandardGameRoomStatus {
+    Waiting,
+    PhotoSelect,
+    CheckRole,
+    Conversation,
+    Vote,
+    Result,
+}
