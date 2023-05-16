3f5c586fd38c5f173adf07ce3a84e4fa8691f431
atamakonkurii <morisudesu@gmail.com>
2023-05-17 07:48:20 +0900

    viewModelの追加

--- app/src/main/java/com/example/photowerewolfandroid/standardGame/viewModel/StandardGameViewModel.kt
+++ app/src/main/java/com/example/photowerewolfandroid/standardGame/viewModel/StandardGameViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.photowerewolfandroid.standardGame.viewModel
 
-class StandardGameViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.photowerewolfandroid.standardGame.utils.StandardGameRoomStatus
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StandardGameViewModel @Inject constructor() : ViewModel() {
+    private val _roomStatus = MutableStateFlow(StandardGameRoomStatus.Waiting)
+    val roomStatus: StateFlow<StandardGameRoomStatus> = _roomStatus
+
+    init {
+        viewModelScope.launch {
+            _roomStatus.value = StandardGameRoomStatus.Waiting
+        }
+    }
 }
